b3a26238b2edf4aefe05f7cebe92446e16a3cff6
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-06-21 02:44:14 +0530

    Add slider to change grid size and other little things

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-const container = document.querySelector('.container');
-const canvasWidth = 25;
-const canvasHeight = 25;
+var canvasWidth = 25;
+var canvasHeight = 25;
 
-for (i = 0; i < canvasWidth * canvasHeight; i++) {
-    const div = document.createElement('div');
-    div.classList.add('pixel');
-    const r = Math.ceil((1/Math.cos(i))*255);
-    const g = Math.ceil((1/Math.sin(i))*255);
-    const b = Math.ceil(Math.random()*Math.sin(i)*255);
+function createCanvas(canvasWidth, canvasHeight){
+    const container = document.createElement('div');
+    container.classList.add('container');
+    for (i = 0; i < canvasWidth * canvasHeight; i++) {
+        const div = document.createElement('div');
+        div.classList.add('pixel');
+        const r = Math.ceil((1/Math.cos(i))*255);
+        const g = Math.ceil((1/Math.sin(i))*255);
+        const b = Math.ceil(Math.random()*Math.sin(i)*255);
+    
+        div.style.width = `${100/canvasWidth}%`;
+        div.style.height = `${100/canvasHeight}%`;
+        div.style.backgroundColor = `rgb(${r}, ${g}, ${g})`;
+        container.appendChild(div);
+    }
+    document.body.appendChild(container);
 
-    div.style.width = `${100/canvasWidth}%`;
-    div.style.height = `${100/canvasHeight}%`;
-    div.style.backgroundColor = `rgb(${r}, ${g}, ${g})`;
-    container.appendChild(div);
 }
+createCanvas(canvasWidth,canvasWidth)
 
-const pixels = document.querySelectorAll('.pixel');
+
+const slider = document.querySelector('#slider');
+slider.addEventListener('input', (inp) =>{
+    canvasWidth = slider.value;
+    console.log(canvasWidth);
+    createCanvas(canvasWidth,canvasWidth)
+    const container = document.querySelector('.container');
+    container.remove();
+    const pixels = document.querySelectorAll('.pixel');
 
 pixels.forEach((pixel)=>{
     //check if mouse is over the pixel
@@ -43,4 +57,5 @@ pixels.forEach((pixel)=>{
           pixel.style.backgroundColor = "gold";
         }
       };
+});
 });
